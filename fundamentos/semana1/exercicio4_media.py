04dd40e4e4701dbdef7040ece921616bd3818f95
https://github.com/gladiston1990/Front-End <gladiston.az@gmail.com>
2026-04-21 08:09:24 -0300

    Exericio 4 - cálculo de média de 3 números

--- fundamentos/semana1/exercicio4_media.py
+++ fundamentos/semana1/exercicio4_media.py
@@ -2,14 +2,16 @@
 #	peça 3 números 
 #	mostre: Média: X
 
-# Exercício Concluído
+# Entrada de dados pelo usuário
 numero_1 = float(input("Digite o número 1: "))
 numero_2 = float(input("Digite o número 2: "))
 numero_3 = float(input("Digite o número 3: "))
 
-media = ((numero_1 + numero_2 + numero_3) / 3)
+# Calculo de soma e média dos valores informados
+soma = numero_1 + numero_2 + numero_3
+media  =  soma / 3
 
-print(f"A média dos números informados é: {media}")
-print(f"Média: {(numero_1 + numero_2 + numero_3) / 3}")
+print(f'Média: {media:.2f}')
+
 
 
