b8820f64bbd8bd1d9a43519fd6e0eacc3c4b12cf
https://github.com/gladiston1990/Front-End <gladiston.az@gmail.com>
2026-04-21 04:25:07 -0300

    Exercício 4 - Calcular Média

---
+++ fundamentos/semana1/exercicio4_media.py
@@ -0,0 +1,15 @@
+#Exercício 4 — Média
+#	peça 3 números 
+#	mostre: Média: X
+
+# Exercício Concluído
+numero_1 = float(input("Digite o número 1: "))
+numero_2 = float(input("Digite o número 2: "))
+numero_3 = float(input("Digite o número 3: "))
+
+media = ((numero_1 + numero_2 + numero_3) / 3)
+
+print(f"A média dos números informados é: {media}")
+print(f"Média: {(numero_1 + numero_2 + numero_3) / 3}")
+
+
