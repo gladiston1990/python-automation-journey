559ffa5110ad087836e6cb520d3ea39614e35358
https://github.com/gladiston1990/Front-End <gladiston.az@gmail.com>
2026-04-21 04:24:19 -0300

    Exercício 3 - Soma

---
+++ fundamentos/semana1/exercicio3_soma.py
@@ -0,0 +1,12 @@
+
+# Exercício 3 — Soma
+#	peça dois números 
+#	mostre a soma 
+# cuidado: usar int() ou float()
+
+numero_1 = float(input("Digite o valor do número 1: "))
+numero_2 = float(input("Digite o valor do número 2: "))
+
+print((numero_1 + numero_2))
+
+
