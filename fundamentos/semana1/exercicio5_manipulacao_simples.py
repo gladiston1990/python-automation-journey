a6b7dc45f001e4d931ad38acd97b551bc2aa1e56
https://github.com/gladiston1990/Front-End <gladiston.az@gmail.com>
2026-04-21 04:26:25 -0300

    Exercício 5 - Calcular o Dobro e Triplo

---
+++ fundamentos/semana1/exercicio5_manipulacao_simples.py
@@ -0,0 +1,12 @@
+#   BLOCO 3 — Manipulação simples
+#   Exercício 5 — Dobro e Triplo
+#   peça um número 
+#   mostre: Dobro: X e Triplo: Y
+
+numero_x = float(input("Digite o valor de X: "))
+numero_y = float(input("Digite o valor de Y: "))
+
+dobro = (numero_x * 2)
+triplo = (numero_y * 3)
+
+print(f"O dobro de x é: {dobro} e o Triplo de Y é: {triplo} ")
