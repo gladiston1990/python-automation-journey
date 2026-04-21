db0f10c89eb3075cb9c3e7d682591077a35932c8
https://github.com/gladiston1990/Front-End <gladiston.az@gmail.com>
2026-04-21 04:44:43 -0300

    Exercício 2 - Leitura de idade com conversão para inteiro

--- fundamentos/semana1/exercicio2_idade.py
+++ fundamentos/semana1/exercicio2_idade.py
@@ -1,7 +1,8 @@
 # Exercício 2 — Idade
 # peça idade, mostre: Você tem 25 anos
 
-idade = input("Quantos anos você tem?")
+# Sugestão da IA - Definir o tipo de dado para prepara para calculos futuros
+idade = int(input("Quantos anos você tem? "))
 
 print(f"Você tem {idade} anos")
 
