ee61e6c3cc0c959bcfc7be867ae7781a6da57090
https://github.com/gladiston1990/Front-End <gladiston.az@gmail.com>
2026-04-21 08:05:23 -0300

    Exercício 3 - soma de dois números com conversão para float

--- fundamentos/semana1/exercicio3_soma.py
+++ fundamentos/semana1/exercicio3_soma.py
@@ -8,6 +8,7 @@ numero_1 = float(input("Digite o valor do número 1: "))
 numero_2 = float(input("Digite o valor do número 2: "))
 
 soma = (numero_1 + numero_2)
+
 print(f"A Soma dos números 1 e 2 é: {soma}")
 
 
